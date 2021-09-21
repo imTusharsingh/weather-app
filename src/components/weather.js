2278118454713006a63a5a358ea86159033114b5
Tushar Singh <rajputtushar309@gmail.com>
2021-09-21 18:35:23 +0530

    Added live location Feature

--- src/components/weather.js
+++ src/components/weather.js
@@ -7,13 +7,63 @@ const Weather = () => {
 
     const [input, setinput] = useState("delhi")
     const [info, setinfo] = useState("")
+    const [userloc, setuserloc] = useState({})
+    const [isinput, setisinput] = useState(false)
 
 
+//------------------------getting live location of user---------------------------------
+
+//checking location is active or not
+    const location = () => {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(geoSuccess, geoError)
+        }
+        else {
+            console.log("location not active")  
+        }
+    }
+
+//getting longitude and latitude of user(location active)   
+    function geoSuccess(position) {
+        const lon = position.coords.longitude;
+        const lat = position.coords.latitude;
+        setuserloc({
+            userlon: lon,
+            userlat: lat
+        })
+    }
+
+//alerting the user and setting random latitude and longitude (location not active)    
+    function geoError() {
+        alert("Geocoder failed.");
+        setuserloc({
+            userlat : 	28.653,
+            userlon : 	77.228
+        })
+    }
+//destructuring the location
+    const { userlon, userlat } = userloc;
+
+    // console.log(userloc)
+
+//--------------------------------------------------------------------------------------
+
+//--------------------------------GETTING THE DATA----------------------------
     const search = async () => {
         try {
-            let url = `https://api.openweathermap.org/data/2.5/weather?q=${input}&units=metric&&appid=9bcf454495f5629f05614b1e44ecb372`
+            let url;
+            if (isinput === true) {
+                url = `https://api.openweathermap.org/data/2.5/weather?q=${input}&units=metric&&appid=8b75858b6d1405245fd495370bf2ff23`
+            }
+            else if (userloc!==undefined) {
+                url = `https://api.openweathermap.org/data/2.5/weather?lat=${userlat}&lon=${userlon}&units=metric&appid=8b75858b6d1405245fd495370bf2ff23`
+            }
+            console.log(url)
+
             let res = await fetch(url);
             let data = await res.json()
+
+//destructuring the data
 
             const { temp, humidity, pressure } = data.main
             const { name, visibility } = data
@@ -21,6 +71,7 @@ const Weather = () => {
             const { country } = data.sys
             const { main } = data.weather[0];
             // console.log(visibility)
+//storing data in state variable
 
             const myNewData = {
                 temp,
@@ -36,36 +87,47 @@ const Weather = () => {
 
             setinfo(myNewData)
             setinput("")
+
         }
         catch (e) {
             console.log(e)
         }
+
     }
 
+//--------------------------------------------------------------------------------------
+
+//-------------------RUNNING THE PROGRAM WHEN PAGE REFRESH-------------------------------
+   
     useEffect(() => {
+        location()
+        if(userlon!==undefined){
+        search()
+            // setisinput(true)
+        }
+    }, [userlon])
 
-        search();
-
-    }, [])
+//----------------------------------------------------------------------------------------
 
     const {
         name,
         main, country } = info
-
-
-
+       
+  
     return (
         <>
-
+       
             <div className="main">
-                <div className="city">{name},{country}</div>
+                <div className="city"><p>{name},{country}</p> 
+                <p className={isinput?"live not":"live"}>LIVE</p>
+                </div>
                 <div className="wrapper">
                     <div className="top">
                         <Icon main={main} />
 
                         <div className="search_container">
                             <input type="text" value={input} onChange={(e) => setinput(e.target.value)} />
-                            <button onClick={search} >
+                            <button onClick={() => { search()}} onMouseOver={()=>{setisinput(true)}}>
                                 <i className="fa fa-search" aria-hidden="true"></i>
                             </button>
                         </div>
